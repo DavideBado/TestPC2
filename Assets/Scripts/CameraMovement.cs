0d49ff24c92224e4f4beb642f96eaf9ae3ff1bf8
Anna Bertolani <bertolani.anna@gmail.com>
2019-11-11 16:22:52 +0100

    inizio camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float turnSpeed;
+    public Transform player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(player.position.x, player.position.y + 3f, player.position.z );
+    }
+
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
