a249c9ca9a453bae0f0178ab07e81f1112d28b21
Anna Bertolani <bertolani.anna@gmail.com>
2019-11-14 15:48:15 +0100

    sistemata cam

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,17 +7,17 @@ public class CameraMovement : MonoBehaviour
     public float turnSpeed;
     public Transform player;
 
-    private Vector3 offset;
-
     void Start()
     {
-        offset = new Vector3(player.position.x, player.position.y + 3f, player.position.z );
+        
     }
 
     void LateUpdate()
     {
-        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-        transform.position = player.position + offset;
-        transform.LookAt(player.position);
+        if (Input.GetMouseButton(0))
+        {
+            transform.RotateAround(player.transform.position, Vector3.up, Input.GetAxis("Mouse X") * turnSpeed); 
+        }
     }
+
 }
