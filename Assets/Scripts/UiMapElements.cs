d8660c4380f9f93124b2082930e5526e5ae1fb61
Anna Bertolani <bertolani.anna@gmail.com>
2019-11-15 18:16:51 +0100

    init drag immagini mappa

---
+++ Assets/Scripts/UiMapElements.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiMapElements : MonoBehaviour
+{
+
+    bool dragging;
+    public GameObject imageToDrag;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && dragging == false)
+        {
+            GameObject newImageToDrag = GameObject.Instantiate(imageToDrag, Vector3.zero, Quaternion.identity);
+            newImageToDrag.transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            dragging = true;
+        }
+        else if (Input.GetMouseButtonUp(0) && dragging == true)
+        {
+            dragging = false;
+        }
+    }
+}
---
+++ Assets/testDrag.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class testDrag : MonoBehaviour, IDragHandler 
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+}
