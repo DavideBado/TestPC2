8a04fa6a6919fa423fbfeff0b67189d1e3155e2e
Davide <davidebadolato@tiscali.it>
2019-11-23 00:54:31 +0100

    Aggiunto main menu

---
+++ Assets/Scripts/LoaderTemporaneo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderTemporaneo : MonoBehaviour
+{
+    public void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Quit()
+    {
+        {
+            Application.Quit();
+        }
+    }
+}
